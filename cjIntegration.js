ce52eb8739d988e187322720862ad1e755505681
Bowie <bowie.leidy@gmail.com>
2018-09-06 17:39:04 -0400

    updating initializeCj

--- cjIntegration.js
+++ cjIntegration.js
@@ -1,7 +1,13 @@
-var today = new Date();
-var cjTracking = ['cjevent'];
-var purchaseURL = 'newtvorder.aspx';
+var initializeCj = function() {
+    var today = new Date();
+    var cjTracking = ['cjevent'];
+    var purchaseURL = 'newtvorder.aspx';
+    var tempPID = getValue('PID');
+    var tempCj = getValue('cjevent');
+    var mutatedPID = tempCj + '-' + tempPID;
 
-waitFor(window.initLinks).then(
-    initLinks(cjTracking, purchaseURL, ['PID', 'cjevent'])
-);
+    setCookie('PID', mutatedPID);
+    initLinks(cjTracking, purchaseURL, mutatedPID);
+};
+
+waitFor(window.initLinks).then(initializeCj);
