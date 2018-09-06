055821b0aa197904319ff0f241e0491d2fb84a4a
Bowie <bowie.leidy@gmail.com>
2018-09-06 19:34:56 -0400

    accounting for various scenarios where we may have to update the values of either the cjevent or the pid

--- cjIntegration.js
+++ cjIntegration.js
@@ -3,16 +3,31 @@ var initializeCj = function() {
     var purchaseURL = 'newtvorder.aspx';
     var tempPID = getValue('PID');
     var tempCj = getValue('cjevent');
-    var mutatedPID = tempPID + '-cjevent-' + tempCj;
-
+    var mutatedPID;
     if (tempPID.indexOf('-cjevent-') != -1) {
         if (isEqual(mutatedPID, tempPID) === false) {
-            tempCj = getParameterByName('cjevent');
-            tempPID = getParameterByName('PID');
-            setCookie('PID', mutatedPID);
+            var oldPID = getSubParameter('PID', '', '-cjevent-');
+            var oldCj = getSubParameter('PID', '-cjevent-', '');
+            var newPID = getParameterByName('PID');
+            var newCj = getParameterByName('cjevent');
+            if (isEqual(newPID, oldPID) === false) {
+                if (newPID != null) {
+                    tempPID = newPID;
+                } else {
+                    tempPID = oldPID;
+                }
+            }
+            if (isEqual(newCj, oldCj) === false) {
+                if (newCj != null) {
+                    tempCj = newCj;
+                } else {
+                    tempCj = oldCj;
+                }
+            }
         }
     }
 
+    mutatedPID = tempPID + '-cjevent-' + tempCj;
     setCookie('PID', mutatedPID);
 
     // initLinks(cjTracking, purchaseURL, mutatedPID);
